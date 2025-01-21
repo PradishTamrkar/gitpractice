5baabd2f2d0a9401e8b50464e7f68dde0e62b8f8
Pradish Tamrakar <tmkr.pradish@gmail.com>
2025-01-21 09:21:52 +0000

    some changes to are made

--- apacheinstallation.sh
+++ apacheinstallation.sh
@@ -4,3 +4,17 @@ sudo apt install apache2 -y
 
 #start apache2 service
 sudo systemctl start apache2
+
+#enable apache2 service
+sudo systemctl enable apache2
+
+#To check the status of apcache2
+
+if [ -f /var/run/apache2/apache2.pid ]
+then
+	echo "apache2 is running"
+else
+then
+	echo "apache2 is not running, starting apache2 now"
+	sudo systemctl start apache2
+	
